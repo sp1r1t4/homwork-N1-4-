5ae52ac0c28b65521aa46fd7b35501e3b9ff8ae0
sp1r1t4 <elisandrboss@gmail.com>
2023-02-15 19:56:02 +0200

    а

---
+++ hw8/list_compr.py
@@ -0,0 +1,9 @@
+# окрываем файл
+with open('file.txt', 'r') as f:
+    lines = f.readlines()
+
+# выводим генератор списка
+result = [ line[line.find("a"):-1].title() for line in lines if line.find('a') != -1]
+# выводим результат
+print(result)
+
